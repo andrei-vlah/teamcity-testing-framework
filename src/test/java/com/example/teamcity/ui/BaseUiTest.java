4db5837c549e4a7a659e9b2a2a5a6f240de4d1d0
Andrei Vlah <vlah.limited@gmail.com>
2024-11-29 20:52:45 +0200

    Tests fix

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -5,15 +5,25 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.example.teamcity.BaseTest;
 import com.example.teamcity.api.config.Config;
+import com.example.teamcity.api.models.AuthModules;
+import com.example.teamcity.api.models.ServerAuthSettings;
 import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.requests.ServerAuthRequest;
+import com.example.teamcity.api.spec.Specifications;
 import com.example.teamcity.ui.pages.LoginPage;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.util.Map;
 
+import static com.example.teamcity.api.generators.TestDataGenerator.generate;
+
 public class BaseUiTest extends BaseTest {
+    private final ServerAuthRequest serverAuthRequest = new ServerAuthRequest(Specifications.superUserSpec());
+    private AuthModules authModules;
+    private boolean perProjectPermissions;
 
     @BeforeSuite(alwaysRun = true)
     public void setupUiTest() {
@@ -30,6 +40,16 @@ public class BaseUiTest extends BaseTest {
                 .screenshots(true)
                 .savePageSource(true)
                 .includeSelenideSteps(true));
+
+        // Получаем текущие настройки perProjectPermissions
+        perProjectPermissions = serverAuthRequest.read().getPerProjectPermissions();
+
+        authModules = generate(AuthModules.class);
+        // Обновляем значение perProjectPermissions на true
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                .perProjectPermissions(true)
+                .modules(authModules)
+                .build());
     }
 
     @AfterMethod(alwaysRun = true)
@@ -39,5 +59,14 @@ public class BaseUiTest extends BaseTest {
 
     protected void loginAs(User user) {
         LoginPage.open().login(user);
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void cleanUpServerAuthSettings() {
+        // Возвращаем настройке perProjectPermissions исходное значение
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                .perProjectPermissions(perProjectPermissions)
+                .modules(authModules)
+                .build());
     }
 }
